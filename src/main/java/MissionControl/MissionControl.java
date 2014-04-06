f398846e072813b59892e8dd4fb14d7148d3deb8
Christina Wettersten <cwetters@mit.edu>
2014-04-06 16:45:13 -0400

    Sub file to publish drive places

---
+++ src/main/java/MissionControl/MissionControl.java
@@ -0,0 +1,19 @@
+package MissionControl;
+
+
+
+
+public class MissionControl extends AbstractNodeMain implements Runnable{
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    
+    
+    
+    
+}
