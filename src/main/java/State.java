bd7ddf7fb9038d73e4d7695700999225fda3aff2
RSS Student <rss-team-6@lists.csail.mit.edu>
2014-04-13 14:48:37 -0400

    adding stub State class

---
+++ src/main/java/State.java
@@ -0,0 +1,68 @@
+package StateMachine;
+
+
+import rss_msgs.PositionMsg;
+import rss_msgs.PositionTargetMsg;
+import rss_msgs.WaypointMsg;
+import rss_msgs.BumpMsg;
+import rss_msgs.BreakBeamMsg;
+import rss_msgs.SonarMsg;
+import rss_msgs.OdometryMsg;
+
+public class State {
+
+private String name
+
+public State (String name){
+	this.name = name;
+
+}
+
+
+public void handle (BumpMsg msg){
+
+	System.out.println("Node " + name + " handled bump msg");
+}
+
+
+public void handle (SonarMsg msg){
+
+	System.out.println("Node " + name + " handled sonar msg");
+}
+
+
+
+public void handle (PositionMsg msg){
+
+	System.out.println("Node " + name + " handled pose msg");
+}
+
+
+public void handle (WaypointMsg msg){
+
+	System.out.println("Node " + name + " handled waypoint msg");
+}
+
+
+public void handle (OdometryMsg msg){
+
+	System.out.println("Node " + name + " handled odo msg");
+}
+
+
+public void handle (BreakBeamMsg msg){
+
+	System.out.println("Node " + name + " handled breakbeam msg");
+}
+
+public void handle (PositionTargetMsg msg){
+
+	System.out.println("Node " + name + " handled postarg msg");
+}
+
+
+
+
+
+
+}
