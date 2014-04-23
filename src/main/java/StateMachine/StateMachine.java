aecb13ebda3f5cc494f9a6ec583fd4701c341871
Christina Wettersten <cwetters@mit.edu>
2014-04-23 09:23:09 -0400

    moar states

--- src/main/java/StateMachine/StateMachine.java
+++ src/main/java/StateMachine/StateMachine.java
@@ -11,6 +11,7 @@ import rss_msgs.WaypointMsg;
 import rss_msgs.BumpMsg;
 import rss_msgs.BreakBeamMsg;
 import rss_msgs.SonarMsg;
+import rss_msgs.BallLocationMsg;
 import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
@@ -32,10 +33,15 @@ public class StateMachine extends AbstractNodeMain implements Runnable {
     public Subscriber<BumpMsg> bumpSub;
     public Subscriber<BreakBeamMsg> breakbeamSub;
     public Subscriber<SonarMsg> sonarSub;
+    public Subscriber<BallLocationMsg> ballLocationSub;
     
     public Publisher<PositionTargetMsg> posTargMsgPub;
     public Publisher<std_msgs.String> ctrlStatePub;
+
     public Publisher<VelocityMsg> velPub;
+
+    public Publisher<BallLocationMsg> ballLocationPub;
+
     
     
     //Probably should be changed to Waypoint..
@@ -78,6 +84,7 @@ public class StateMachine extends AbstractNodeMain implements Runnable {
 
     private State currState;
     
+
     /*private State state = new State ("example"){
 	    @Override
 		public void handle (PositionMsg msg){
@@ -143,20 +150,262 @@ public class StateMachine extends AbstractNodeMain implements Runnable {
     private State stopState = new State("stop"){
 	};
 	
+//////////////////////////////////////////////////////////////////////////////////States below, not integrated stuff above
+    private final long WANDER_TIME = 240000; //4 minutes
+    
+    private State lastState;
+    private State state;
+    
+    
+    private State example = new State ("example"){
+      @Override
+      public void handle (PositionMsg msg){
+          System.out.println("I'm an overwritten state handler for pose");
+      }
+    };
+    
+    /**
+     * States for Gather phase:
+     * lost - find yourself. Only goes to whatever you were doing last/spin
+     * spin - look around for block. goes to visualServo on a BallLocationMsg; after 2 spins goes to wander
+     * visualServo - visualServo to a block. Goes to spin or drive depending on where it came from
+     * wander - wander to a new location. Goes to visualServo on a BallLocationMsg
+     * 
+     * exit states:
+     * if enough time has passed, and if the robot is in spin or drive, go to driveEnter state 
+     */
+    private State lost = new State("lost"){
+        @Override
+        public void handle(PositionMsg msg){
+            if(localized(msg)){
+                state = lastState;
+                lastState = lost;
+            }
+        }
+    };
+    
+    private State spin = new State("spin"){
+      int spins = -1;
+      double startTheta = 0;
+      final double SPIN_THRESHOLD = 0.5;
+      @Override
+      public void handle(PositionMsg msg){ 
+          if (spins == -1){
+              startTheta = msg.getTheta();
+          }
+          if ((msg.getTheta - startTheta)%360 < SPIN_THRESHOLD){ //probs a better way to do this
+              spins++;   
+          }
+          if (spins > 2){
+              state = wander;
+              lastState = spin; 
+              publishWander();
+          }
+      }
+      
+      @Override
+      public void handle(BallLocationMsg msg){
+          state = visualServo;
+          lastState = spin;
+                  
+      }
+    };
+    
+    private State visualServo = new State("visualServo"){
+       @Override
+       public void handle(BallLocationMsg msg){
+           std_msgs.String ctrlState = ctrlStatePub.newMessage();
+           ctrlState.setData("visualServo");
+           ctrlStatePub.publish(ctrlState);
+           
+           BallLocationMsg ballMsg = ballLocationPub.newMessage();
+           
+           ballMsg.setRange(0);
+           ballMsg.setBearing(0);
+           ballLocatoinPub.publish(ballMsg);
+           
+           //check if ball is no longer in frame, drive forward extra x feet, return to previous state
+           
+       }
+    };
+    
+    private State wander = new State("wander"){
+        @Override
+        final double WANDER_THRESHOLD = 0.5;
+        public void handle(PositionMsg msg){
+            if (timeToGoHome()){
+                state = driveBuildSite;
+                lastState = driveLost;
+                PositionTargetMsg targmsg = posTargMsgPub.newMessage();
+                targmsg.setX(0);
+                targmsg.setY(0);
+                targmsg.setTheta(0);
+                currGoal = targmsg;
+                posTargMsgPub.publish(targmsg);
+                return;
+            }
+            
+            if(Math.sqrt(Math.pow(msg.getX() - currGoal.getX(), 2) + Math.pow(msg.getY() - currGoal.getY(), 2))< WANDER_THRESHOLD){
+                publishWander();
+            }
+        }
+        
+        public void handle(BallLocationMsg msg){
+            state = visualServo;
+            lastState = wander;
+        }
+    };
+    
+    private void publishWander(){
+        PositionTargetMsg msg = posTargMsgPub.newMessage();
+        msg.setX(Math.random()*3 - .5);
+        msg.setY(Math.random()*4.5 - .5);
+        currGoal = msg;
+        posTargMsgPub.publish(msg);    
+    }
+    
+    /**
+     * States for Drive to build site
+     * driveBuildSite - drives home. 
+     * driveLost - find yourself. Goes back to driveBuildSite
+     * 
+     * 
+     * exit states:
+     * when you're at the build site go to buildXX
+     */
+    
+    private State driveLost = new State ("driveLost"){
+        @Override
+        public void handle(PositionMsg msg){
+            if(localized(msg)){
+                state = lastState;
+                lastState = driveLost;
+            }
+        }
+    };
+    
+    private State driveBuildSite = new State("driveBuildSite"){
+        @Override
+        final double DISTANCE_THRESHOLD = 0.5;
+        final double HOME_X = 0.0;
+        final double HOME_Y = 0.0;
+        public void handle(PositionMsg msg){
+            if (Math.sqrt(Math.pow(HOME_X - msg.getX(), 2) + Math.pow(HOME_Y - msg.getY(),2)) < DISTANCE_THRESHOLD){
+                state = buildEnter;
+                lastState = buildLost;
+            }
+        }
+    };
+    
+    /**
+     * States for building stuff: what needs to happen here?
+     */
+    
+    private State buildEnter = new State("buildEnter"); //what are we gonna build?
+    
+    private State buildLost = new State("buldLost"); //does this need to exist?
+    
+    public boolean localized(PositionMsg msg){
+        //@TODO:check if msg has enough confidence to mean we are sure we know where we are
+        //@TODO: keep track of position updates, check to see if positon is changing to rapidly 
+        return true;
+    }
+    
+    public double dist(PositionMsg odo){
+        // not sure how you want to deal with this more nicely, this is hackish --bhomberg
+        if(currGoal == null)
+            return Double.POSITIVE_INFINITY;
+            return Math.sqrt(Math.pow(odo.getX() - currGoal.getX(), 2) + 
+                                Math.pow(odo.getY() - currGoal.getY(), 2));
+            
+    }
+    
+    /**
+     * Get time since start of execution in ms.
+     */
+    public long getTime(){
+        return System.currentTimeMillis() - startTime;
+    }
+    
+    public boolean timeToGoHome(){
+        return getTime() > WANDER_TIME;
+    }
+
+    
+///////////////////////////////////////////////////////////////////////////////////////////////end States
     
     
     private Random rand; // for testing --bhomberg
     
     private int count;
    
-    public double dist(PositionMsg odo){
-	// not sure how you want to deal with this more nicely, this is hackish --bhomberg
-	if(currGoal == null)
-	    return Double.POSITIVE_INFINITY;
-        return Math.sqrt(Math.pow(odo.getX() - currGoal.getX(), 2) + 
-                            Math.pow(odo.getY() - currGoal.getY(), 2));
+////////////////////////////////////////////////////////////////////////////////handlers
+
+    public void handle(PositionMsg odo){
+        //IMPLEMENT_STATES
+        //state.handle(odo);
+       
+        if(dist(odo) < ACCEPTABLE_ERROR){
+            PositionTargetMsg msg = posTargMsgPub.newMessage();
+            msg.setX(Math.random()*5.0);
+            msg.setY(Math.random()*5.0);
+            posTargMsgPub.publish(msg);
+            currGoal = msg;
         
+        }
+        System.out.println("odo message handled x: " + odo.getX() + 
+                                                "  y: " + odo.getY() + 
+                                                "  theta: " + odo.getTheta());
     }
+    
+    public void handle(WaypointMsg way){
+        //IMPLEMENT_STATES
+        //state.handle(way);
+        
+        System.out.println("waypoint message handled x: " + way.getX() +
+                                                        "  y: " + way.getY() +
+                                                        "  theta: " + way.getTheta());
+        //send waypoint since motion isn't updated yet and I don't know what it's going to be
+        /**
+        PositionTargetMsg msg = motorsPub.newMessage();
+        msg.setX(way.getX());
+        msg.setY(way.getY());
+        msg.setTheta(way.getTheta());
+        motorsPub.publish(msg);
+	*/
+        //end hacks
+    }
+    
+    public void handle(BumpMsg bump){
+        //IMPLEMENT_STATES
+        //state.handle(bump);
+        
+        System.out.println("bump message handled");
+	count++;
+
+	if(count >= 20){
+	    PositionTargetMsg msg = posTargMsgPub.newMessage();
+	    msg.setX(-0.1524);
+	    msg.setY(3.6576);
+	    //msg.setX(rand.nextDouble()*3 - 0.5);
+	    //msg.setY(rand.nextDouble()*4.5 - 0.5);
+	    msg.setTheta(-1);
+	    posTargMsgPub.publish(msg);
+	    count = 0;
+	}
+    }
+    public void handle(BreakBeamMsg bbeam){
+         //IMPLEMENT_STATES
+        //state.handle(bbeam);
+        System.out.println("bbeam handled");
+    }
+    public void handle(SonarMsg sonar){
+        //IMPLEMENT_STATES
+        //state.handle(sonar);
+        System.out.println("sonar handled");
+    }
+////////////////////////////////////////////////////////////////////////////////end handlers
+   
 
     @Override
     public void run() {
@@ -179,13 +428,7 @@ public class StateMachine extends AbstractNodeMain implements Runnable {
 	    }*/
     }
     
-    /**
-     * Get time since start of execution in ms.
-     */
-    public long getTime(){
-        return System.currentTimeMillis() - startTime;
-    }
-
+    
     /**
      * <p>
      * Run the VisualServo process
@@ -202,8 +445,12 @@ public class StateMachine extends AbstractNodeMain implements Runnable {
 	posTargMsgPub = node.newPublisher("/state/PositionTarget", "rss_msgs/PositionTargetMsg");
 	ctrlStatePub = node.newPublisher("/state/State", std_msgs.String._TYPE);
 	waypointPub = node.newPublisher("/path/Waypoint", "rss_msgs/WaypointMsg");
+
 	velPub = node.newPublisher("/state/Velocity", "rss_msgs/VelocityMsg");
 	//rand = new Random();
+
+	ballLocationPub = node.newPublisher("/state/BallLocation", "rss_msgs/BallLocationMsg");
+
 
 	//yay hacks to see if things work
 	
@@ -260,8 +507,20 @@ public class StateMachine extends AbstractNodeMain implements Runnable {
         });
 
 
+
 	currState = startState;
         //count = 0; // for testing --bhomberg
+
+        
+        ballLocationSub = node.newSubscriber("/vis/BallLocation", "rss_msgs/BallLocationMsg");
+        ballLocationSub.addMessageListener(new MessageListener<rss_msgs.BallLocationMsg>() {
+            @Override
+            public void onNewMessage(rss_msgs.BallLocationMsg message){
+                handle(message);
+                System.out.println("State Machine got a ball location message");
+            }
+        });
+        
     }
     
         
